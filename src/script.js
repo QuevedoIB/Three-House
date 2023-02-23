0bc50c38e645dabafba8c2cab9eaaa2f2e184ccd
4esoquevedoi@gmail.com <4esoquevedoi@gmail.com>
2023-02-23 21:10:19 +0100

    Add door light

--- src/script.js
+++ src/script.js
@@ -110,7 +110,7 @@ door.translateY(-0.1);
 
 const roof = new THREE.Mesh(
   new THREE.ConeGeometry(walls.geometry.parameters.width - 1, 1, 4, 1),
-  new THREE.MeshBasicMaterial({ color: 0xd06d43, map: brickColor })
+  new THREE.MeshStandardMaterial({ color: 0xd06d43, map: brickColor })
 );
 
 roof.rotation.y = Math.PI * 0.25;
@@ -198,7 +198,7 @@ for (let i = cornerUpperRight.x; i > maxDoorOffset; i -= bushSize * 2) {
  * GRAVEYARDS
  */
 
-const graveyardAmount = 50;
+const graveyardAmount = 20;
 const graveyards = new THREE.Group();
 
 const graveyardHeight = 0.8;
@@ -211,8 +211,12 @@ for (let i = 0; i < graveyardAmount; i++) {
   const offsetRadius = wallsSize + bushSize + Math.random() * (wallsSize * 0.5);
   const x = Math.sin(angle) * offsetRadius;
   const z = Math.cos(angle) * offsetRadius;
+  const y = floor.position.y + graveyardHeight * 0.5;
+  const zRotation = (Math.random() - 0.5) * 0.2;
 
-  graveyard.position.set(x, floor.position.y + graveyardHeight * 0.5 + 0.01, z);
+  graveyard.position.set(x, y, z);
+  graveyard.rotation.y = Math.random() * Math.PI * 0.5;
+  graveyard.rotation.z = zRotation;
 
   graveyards.add(graveyard);
 }
@@ -222,18 +226,26 @@ scene.add(graveyards);
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+const ambientLight = new THREE.AmbientLight("#b9d5ff", 0.12);
 gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const moonLight = new THREE.DirectionalLight("#ffffff", 0.5);
+const moonLight = new THREE.DirectionalLight("#b9d5ff", 0.12);
 moonLight.position.set(4, 5, -2);
 gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
 gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(moonLight);
+
+const doorLight = new THREE.PointLight("#ff7d46", 1, 7);
+doorLight.position.set(
+  door.position.x * 2,
+  door.position.y * 2,
+  door.position.z
+);
+scene.add(doorLight);
 
 /**
  * Sizes
